5bfd32efbab1fa196487136ef0e8daf268b4f36e
8ball <originalgangsterboss@protonmail.com>
2021-03-20 18:12:15 -0700

    test

--- src/components/TokenSymbol/TokenSymbol.tsx
+++ src/components/TokenSymbol/TokenSymbol.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import BNBLogo from '../../assets/img/bnb.png'
 import ACSLogo from '../../assets/img/acs.png'
-import Farmer from '../../assets/img/farmer.png'
+import Farmer from '../../assets/img/acs.png'
 
 
 const logosBySymbol: {[title: string]: string} = {
