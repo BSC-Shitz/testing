db3d65175e4da16a74fc4360722dc1d4c8947e92
yoyman <yoyman@users.noreply.github.com>
2021-04-14 12:10:38 -0400

    Data organization and testing

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -5,7 +5,7 @@ import PageHeader from '../../components/PageHeader';
 const Home: React.FC = () => {
   return (
     <Page>
-      <PageHeader icon="💩" subtitle="BSC SHITZ is a token on the Binance Smart Chain offering duration based staking pools for SHITZ/BNB Liquidity Providers along with incentivizing SHITZ Token holders with different kinds of contests and prizes." title="WELCOME" />
+      <PageHeader icon="💩" title="WELCOME to SHITZ" subtitle="BSC SHITZ is a token on the Binance Smart Chain offering duration based staking pools for SHITZ/BNB Liquidity Providers along with incentivizing SHITZ Token holders with different kinds of contests and prizes." />
     </Page>
   );
 };
